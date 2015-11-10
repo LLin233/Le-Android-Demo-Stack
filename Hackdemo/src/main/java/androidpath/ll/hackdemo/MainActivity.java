11b575a4d9244793109d5fb854d4069a7966ae92
linle <linle.job@gmail.com>
2015-11-09 16:30:58 -0800

    android tricks

---
+++ Hackdemo/src/main/java/androidpath/ll/hackdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package androidpath.ll.hackdemo;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

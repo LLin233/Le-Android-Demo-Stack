a90fbbda257422da8dac998a2628ec2d687183e1
linle <linle.job@gmail.com>
2015-11-10 20:01:33 -0800

    attach butterknife to Activity

--- Hackdemo/src/main/java/androidpath/ll/hackdemo/MainActivity.java
+++ Hackdemo/src/main/java/androidpath/ll/hackdemo/MainActivity.java
@@ -2,13 +2,22 @@ package androidpath.ll.hackdemo;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btn_lazy_loading)
+    public void goToLazyLoadingDemo(View view) {
+        actionStart(MainActivity.this, TestActivity.class);
     }
 }
